9d45699ef1af28cfb7e12c643b77ea8e970edbe6
Andreas Schawo <andreas@schawo.de>
2010-06-17 15:30:07 +0200

    Added a wxWidgets GUI

---
+++ SVNExportGUI.py
@@ -0,0 +1,106 @@
+import os
+import sys
+
+import wx
+import wx_svnexport
+
+from svnexport import *
+
+__version__ = 'v0.1'
+__author_name__ = 'Andreas Schawo'
+__author_email__ = 'andreas@schawo.de'
+__copyright__ = 'Copyright (c) 2010, Andreas Schawo, All rights reserved'
+__license__ = '''Copyright (c) 2010, Andreas Schawo <andreas@schawo.de>
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+   2. Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+   3. Neither the name of the authors nor the names of its contributors may
+      be used to endorse or promote products derived from this software without
+      specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHORS 'AS IS' AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.'''      
+
+class SVNExportFrame( wx_svnexport.Frame ):
+    def __init__(self, parent):
+        wx_svnexport.Frame.__init__(self, parent)
+
+        try:
+            icon = wx.Icon('svnexport.ico', wx.BITMAP_TYPE_ICO, 32, 32)
+            self.SetIcon(icon)
+        except:
+            pass
+
+        self.m_dirPickerPath.SetPath(os.path.join(os.getcwd(), 'export'))
+        
+        self.Bind(wx.EVT_BUTTON, self.OnExport, self.m_buttonExport)
+        self.Bind(wx.EVT_BUTTON, self.OnExit, self.m_buttonExit)
+
+        self.m_timer = wx.Timer(self, wx.ID_ANY)
+        self.Bind(wx.EVT_TIMER, self.OnTimer, self.m_timer)
+
+    def _SetEnabled(self, state=True):
+        self.m_buttonExport.Enable(state)
+
+    def _SetDisabled(self):
+        self._SetEnabled(False)
+
+    def OnTimer(self, evt):
+        if len(self._entries) > 0:
+            entry = self._entries.pop(0)
+            export_entry(self.rurl, self.epath, entry)
+            self.m_gaugeProgress.SetValue(self.m_gaugeProgress.GetValue()+1)
+            self.m_timer.Start(1, True)
+        else:
+            self._SetEnabled()
+        
+    def OnExport(self, evt):
+        self.rurl = self.m_textCtrlURL.GetValue()
+        self.epath = self.m_dirPickerPath.GetPath()
+        entry_rev = self.m_checkBoxEntryRev.IsChecked()
+
+        self.m_gaugeProgress.SetValue(0)
+
+        if not os.path.exists(self.epath):
+            os.mkdir(self.epath)
+        elif not len(os.listdir(self.epath)) == 0:
+            self.m_statusBar.SetStatusText('Folder "%s" is not empty!' %self.epath)
+            return
+
+        self._entries = list(get_entries(self.rurl, entry_rev))
+
+        if len(self._entries) > 0:
+            self.m_gaugeProgress.SetRange(len(self._entries))
+            self._SetDisabled()
+            self.m_timer.Start(1, True)
+        
+    def OnExit(self, evt):
+        self.Close()
+
+class SVNExportApp(wx.App):
+    def OnInit(self):
+        frame = SVNExportFrame(None)
+        frame.Show(True)
+        self.SetTopWindow(frame)
+        return True
+
+
+if __name__ == '__main__':
+    app = SVNExportApp(False)
+    app.MainLoop()
---
+++ wx_svnexport.py
@@ -0,0 +1,71 @@
+###########################################################################
+## Python code generated with wxFormBuilder (version Jun 11 2009)
+## http://www.wxformbuilder.org/
+##
+## PLEASE DO "NOT" EDIT THIS FILE!
+###########################################################################
+
+import wx
+
+###########################################################################
+## Class Frame
+###########################################################################
+
+class Frame ( wx.Frame ):
+	
+	def __init__( self, parent ):
+		wx.Frame.__init__  ( self, parent, id = wx.ID_ANY, title = u"SVN-Export", pos = wx.DefaultPosition, size = wx.Size( 362,310 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL, name = u"SVN-Export" )
+		
+		self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
+		self.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_MENU ) )
+		
+		bSizer1 = wx.BoxSizer( wx.VERTICAL )
+		
+		self.m_staticText1 = wx.StaticText( self, wx.ID_ANY, u"URL to repository", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText1.Wrap( -1 )
+		bSizer1.Add( self.m_staticText1, 0, wx.LEFT|wx.TOP, 5 )
+		
+		self.m_textCtrlURL = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( -1,-1 ), 0 )
+		bSizer1.Add( self.m_textCtrlURL, 0, wx.ALL|wx.EXPAND, 5 )
+		
+		self.m_staticText2 = wx.StaticText( self, wx.ID_ANY, u"Path to export to", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText2.Wrap( -1 )
+		bSizer1.Add( self.m_staticText2, 0, wx.LEFT|wx.TOP, 5 )
+		
+		self.m_dirPickerPath = wx.DirPickerCtrl( self, wx.ID_ANY, wx.EmptyString, u"Select a folder", wx.DefaultPosition, wx.DefaultSize, wx.DIRP_USE_TEXTCTRL )
+		bSizer1.Add( self.m_dirPickerPath, 0, wx.ALL|wx.EXPAND, 5 )
+		
+		self.m_checkBoxEntryRev = wx.CheckBox( self, wx.ID_ANY, u"Use entry revision instead of repository", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer1.Add( self.m_checkBoxEntryRev, 0, wx.ALL, 5 )
+		
+		
+		bSizer1.AddSpacer( ( 0, 0 ) )
+		
+		self.m_gaugeProgress = wx.Gauge( self, wx.ID_ANY, 100, wx.DefaultPosition, wx.Size( -1,20 ), wx.GA_HORIZONTAL )
+		self.m_gaugeProgress.SetValue( 0 ) 
+		bSizer1.Add( self.m_gaugeProgress, 0, wx.ALL|wx.EXPAND, 5 )
+		
+		
+		bSizer1.AddSpacer( ( 0, 0 ) )
+		
+		bSizer2 = wx.BoxSizer( wx.HORIZONTAL )
+		
+		
+		bSizer2.AddSpacer( ( 0, 0 ) )
+		
+		self.m_buttonExport = wx.Button( self, wx.ID_ANY, u"Export", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer2.Add( self.m_buttonExport, 0, wx.ALIGN_BOTTOM|wx.ALIGN_RIGHT|wx.ALL, 5 )
+		
+		self.m_buttonExit = wx.Button( self, wx.ID_ANY, u"Exit", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer2.Add( self.m_buttonExit, 0, wx.ALIGN_BOTTOM|wx.ALIGN_RIGHT|wx.ALL, 5 )
+		
+		bSizer1.Add( bSizer2, 1, wx.ALIGN_BOTTOM|wx.ALIGN_RIGHT|wx.EXPAND, 5 )
+		
+		self.SetSizer( bSizer1 )
+		self.Layout()
+		self.m_statusBar = self.CreateStatusBar( 1, wx.ST_SIZEGRIP, wx.ID_ANY )
+	
+	def __del__( self ):
+		pass
+	
+
