5ba4269590768f74853809b30a21eb811d005a56
Andreas Schawo <andreas@schawo.de>
2010-06-22 06:55:59 +0200

    Handle new SVNExportException and fixed closing

--- SVNExportGUI.py
+++ SVNExportGUI.py
@@ -54,47 +54,56 @@ class SVNExportFrame( wx_svnexport.Frame ):
         self.Bind(wx.EVT_BUTTON, self.OnList, self.m_buttonList)
         self.Bind(wx.EVT_BUTTON, self.OnExport, self.m_buttonExport)
         self.Bind(wx.EVT_BUTTON, self.OnExit, self.m_buttonExit)
+        self.Bind(wx.EVT_CLOSE, self.OnExit, self)
         self.Bind(wx.EVT_MENU, self.OnAbout, self.m_menuAbout)
 
         self.m_timer = wx.Timer(self, wx.ID_ANY)
         self.Bind(wx.EVT_TIMER, self.OnTimer, self.m_timer)
 
         self.max_urls = 5
-
+        self.__closing__ = False
+        
         self.__read_config__()
         
     def __read_config__(self):
         self.config_file = os.path.join(os.path.expanduser('~'), '.svnexport.xml')
 
         if os.path.isfile(self.config_file):
-            self.config_dom = xml.dom.minidom.parse(self.config_file)
+            try:
+                self.config_dom = xml.dom.minidom.parse(self.config_file)
 
-            root = self.config_dom.documentElement
+                root = self.config_dom.documentElement
 
-            lists = root.getElementsByTagName('gui')[0].getElementsByTagName('lists')[0]
-            #Read the recent urls
-            for u in lists.getElementsByTagName('url'):
-                self.m_comboBoxURL.Append(u.childNodes[0].data.strip())
-            #self.m_comboBoxURL.SetSelection(0)
+                lists = root.getElementsByTagName('gui')[0].getElementsByTagName('lists')[0]
+                #Read the recent urls
+                for u in lists.getElementsByTagName('url'):
+                    self.m_comboBoxURL.Append(u.childNodes[0].data.strip())
+                #self.m_comboBoxURL.SetSelection(0)
 
-            #Read the last path used
-            pathl = lists.getElementsByTagName('path')
-            if len(pathl) > 0:
-                self.m_dirPickerPath.SetPath(pathl[0].childNodes[0].data.strip())
+                #Read the last path used
+                pathl = lists.getElementsByTagName('path')
+                if len(pathl) > 0:
+                    self.m_dirPickerPath.SetPath(pathl[0].childNodes[0].data.strip())
+            except:
+                self.m_statusBar.SetStatusText('Damaged config file! Generating empty config...')
+                self.__generate_config__()
         else:
-            #Create an empty structure
-            self.config_dom = xml.dom.minidom.getDOMImplementation().createDocument(None, 'svnexport', None)
-            
-            root = self.config_dom.documentElement
+            self.__generate_config__()
 
-            gui_node = self.config_dom.createElement('gui')
-            lists_node = self.config_dom.createElement('lists')
+    def __generate_config__(self):
+        #Create an empty structure
+        self.config_dom = xml.dom.minidom.getDOMImplementation().createDocument(None, 'svnexport', None)
+        
+        root = self.config_dom.documentElement
 
-            root.appendChild(gui_node)
-            gui_node.appendChild(lists_node)
+        gui_node = self.config_dom.createElement('gui')
+        lists_node = self.config_dom.createElement('lists')
 
-            with open(self.config_file, 'w') as f:
-                self.config_dom.writexml(f)
+        root.appendChild(gui_node)
+        gui_node.appendChild(lists_node)
+
+        with open(self.config_file, 'w') as f:
+            self.config_dom.writexml(f)
 
     def __write_config__(self):        
         lists = self.config_dom.documentElement.getElementsByTagName('gui')[0].getElementsByTagName('lists')[0]
@@ -223,8 +232,13 @@ class SVNExportFrame( wx_svnexport.Frame ):
             self.m_timer.Start(1, True)
         
     def OnExit(self, evt):
-        self.__write_config__()
-        self.Close()
+        if self.__closing__:
+            evt.Skip()
+        else:
+            self.__write_config__()
+            self.__closing__ = True
+            self.Close()
+
 
 class SVNExportApp(wx.App):
     def OnInit(self):
