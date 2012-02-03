b6fa97a4c0cab5da1b5c936b68dc98e0baa2e401
Andreas Schawo <andreas@schawo.de>
2012-02-03 16:59:19 +0100

    Switched to Python 2.7

--- make.bat
+++ make.bat
@@ -1,6 +1,6 @@
 @echo off
-set PATH=.\bin;C:\Python26;C:\Programme\git\bin;C:\Programme\wxFormbuilder;C:\Programme\Inno Setup 5;C:\Programme\POEdit\bin
-set TOOL_PATH=C:/Python26/Tools/i18n/
+set PATH=.\bin;C:\Python27;C:\Programme\git\bin;C:\Programme\wxFormbuilder;C:\Programme\Inno Setup 5;C:\Programme\POEdit\bin
+set TOOL_PATH=C:/Python27/Tools/i18n/
 set PYGETTEXT=PYGETTEXT=%TOOL_PATH%pygettext.py
 set MSGFMT=MSGFMT=%TOOL_PATH%msgfmt.py
 
